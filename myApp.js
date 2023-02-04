56f63364d93d2e81aac68cec5536fdbf5659e229
Taylor Lo <97694902+taylor50737@users.noreply.github.com>
2023-02-04 15:13:24 -0500

    test

--- myApp.js
+++ myApp.js
@@ -1,5 +1,37 @@
 let express = require('express');
 let app = express();
-console.log("Hello World");
+require("dotenv").config();
 
- module.exports = app;
+// #1
+// console.log("Hello World");
+
+// #2
+// app.get("/", (req, res) => {
+//   res.send("Hello Express");
+// })
+
+// #3
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+// #4
+app.use("/public", express.static(__dirname + "/public"));
+
+// #5
+// app.get("/json", (req, res) => {
+//   res.json({"message": "Hello json"});
+// });
+
+// #6
+const mySecret = process.env["MESSAGE_STYLE"]
+app.get("/json", (req, res) => {
+  if ( mySecret == "uppercase" ) {
+    res.json({ "message": "HELLO JSON" });
+  }
+  else {
+    res.json({ "message": "Hello json" });
+  }
+});
+
+module.exports = app;
